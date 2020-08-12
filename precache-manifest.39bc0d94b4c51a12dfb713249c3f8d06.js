60e005b7b028e5fdb801cd99c015e492f66b537f
SalmanSayeed79 <66307068+SalmanSayeed79@users.noreply.github.com>
2020-08-12 20:54:27 +0600

    uploaded static files

---
+++ precache-manifest.39bc0d94b4c51a12dfb713249c3f8d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fe4055163517a71135a5c1453ef99b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37fb2ed54b604746af78",
+    "url": "./static/css/main.714b3c53.chunk.css"
+  },
+  {
+    "revision": "f393acc50c679ae9d62c",
+    "url": "./static/js/2.676aee0d.chunk.js"
+  },
+  {
+    "revision": "31b65397dc8a13083a81efedadb0f77a",
+    "url": "./static/js/2.676aee0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fb2ed54b604746af78",
+    "url": "./static/js/main.ace72b97.chunk.js"
+  },
+  {
+    "revision": "289b9c5cb7063410cc19",
+    "url": "./static/js/runtime-main.b8482106.js"
+  }
+]);
